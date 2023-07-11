23d3313aaaa9e23742d5d1a50c06b37551865be6
unknown <lukeharriman1013@gmail.com>
2023-07-11 14:31:02 -0700

    event listener button

--- Develop/script.js
+++ Develop/script.js
@@ -25,25 +25,14 @@ $(function () {
         }
     })
 };
+//event listener when savebtn is clicked
+$('.saveBtn').on("click", function() {
+  //grabs time from hour class of specicic timeBlock when btn is clicked
+    var time = $(this).siblings(".hour").text();
+    //grabs text from description class for each
+    var description = $(this).siblings(".description").val();
 
-    
+    localStorage.setItem(time, description);
+});
 
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
-
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  timeBlock()
 });
